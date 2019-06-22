c566e2512d4613666332402ebc71daf08638b085
Drzaln <drzaln@null.net>
2019-06-23 04:14:28 +0700

    update

--- connect.js
+++ connect.js
@@ -1,10 +1,17 @@
 const sql = require('mysql')
 
+// const connect = sql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'myapp'
+// })
+
 const connect = sql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'myapp'
+    host: 'fdb26.awardspace.net',
+    user: '3078762_myapp',
+    password: 'myapp123.',
+    database: '3078762_myapp'
 })
 
 connect.connect((err) => {
